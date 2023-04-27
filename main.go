068d1506a0353fb076f8e941309ff782953ae3a2
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-04-26 19:21:04 -0700

    Make this a general proxy to solve CORS issues

--- main.go
+++ main.go
@@ -7,22 +7,15 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
-
-// TODO: Look at providing a namespace instead of Host, Path, Protocol and
-// have the service figure out the route to Argo instead of the console plugin.
-// I'm a bit concerned that this service being used to exfiltrate OAuth Tokens
-type endpointInfo struct {
-	Host     string `json:"host"`
-	Path     string `json:"path"`
-	Protocol string `json:"protocol"`
-}
 
 type tokenResponse struct {
 	AccessToken     string `json:"access_token"`
@@ -33,30 +26,63 @@ type tokenResponse struct {
 
 func main() {
 	router := gin.Default()
-	router.POST("/token", exchangeToken)
 
-	//router.Run("localhost:8080")
+	router.GET("/*ignore", ReverseProxy())
+	router.POST("/*ignore", ReverseProxy())
+
+	router.Run("localhost:8080")
 	// TODO: Put certs in better spot like /etc/ssl/certs
-	router.RunTLS("0.0.0.0:8443", "/mnt/certs/tls.crt", "mnt/certs/tls.key")
+	//router.RunTLS("0.0.0.0:8443", "/mnt/certs/tls.crt", "mnt/certs/tls.key")
+
 }
 
-func exchangeToken(c *gin.Context) {
-	var endpoint endpointInfo
+// Generic reverse proxy for Argo CD APIs
+// Will automatically perform a token exchange for the OAuth token
+// that the console is passing in
+func ReverseProxy() gin.HandlerFunc {
 
-	if err := c.BindJSON(&endpoint); err != nil {
-		log.Println(fmt.Errorf("Got error %s", err.Error()))
-		c.JSON(403, err.Error())
-		return
+	return func(c *gin.Context) {
+		director := func(req *http.Request) {
+			host := getArgoServerHost(c.GetHeader("namespace"), os.Getenv("SUBDOMAIN"))
+
+			log.Println(fmt.Printf("%s https://%s", c.Request.Method, host+c.Request.RequestURI))
+
+			req.URL.Scheme = "https"
+			req.URL.Host = host
+
+			token, err := exchangeToken(c)
+			if err != nil {
+				req.Header.Set("Authentication", "Bearer "+token)
+			} else {
+				log.Println("Unexpected error retrieving token", err)
+			}
+			delete(req.Header, "namespace")
+
+		}
+		proxy := &httputil.ReverseProxy{Director: director}
+		proxy.ServeHTTP(c.Writer, c.Request)
 	}
+}
 
-	log.Println(fmt.Printf("Retrieved endpoint\n%s", endpoint))
-
-	token := c.GetHeader("Authorization")[7:]
-	if len(token) == 0 {
-		c.JSON(403, errors.New("Authorization header expected"))
+// Get location of Argo Server Host based on namespace requested.
+// The namespace is passed in by the front-end via a Header
+func getArgoServerHost(namespace string, subdomain string) string {
+	if namespace == "openshift-gitops" {
+		return "openshift-gitops-server-openshift-gitops." + subdomain
+	} else {
+		// Making instance that argo instance in other instances are called argocd
+		// Probably want to look up the route as a better way
+		return "argocd-server-" + namespace + "." + subdomain
 	}
+}
 
-	log.Println(token)
+func exchangeToken(c *gin.Context) (string, error) {
+
+	token := c.GetHeader("Authorization")
+	if len(token) < 8 {
+		return "", errors.New("Invalid authorization header in request")
+	}
+	token = token[7:]
 
 	data := url.Values{}
 	data.Set("grant_type", "urn:ietf:params:oauth:grant-type:token-exchange")
@@ -72,36 +98,33 @@ func exchangeToken(c *gin.Context) {
 		Timeout: time.Second * 10,
 	}
 
-	req, err := http.NewRequest("POST", endpoint.Protocol+"://"+endpoint.Host+endpoint.Path, strings.NewReader(encodedData))
+	host := getArgoServerHost(c.GetHeader("namespace"), os.Getenv("SUBDOMAIN"))
+	req, err := http.NewRequest("POST", "https://"+host+"/api/dex/token", strings.NewReader(encodedData))
 	if err != nil {
 		log.Println(fmt.Errorf("Error constructing request %s", err.Error()))
-		c.JSON(500, err)
-		return
+		return "", err
 	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
 	response, err := client.Do(req)
 	if err != nil {
 		log.Println(fmt.Errorf("Error calling %s", err.Error()))
-		c.JSON(500, err)
-		return
+		return "", err
 	}
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		log.Println(fmt.Errorf("Error reading body %s", err.Error()))
-		c.JSON(500, err)
-		return
+		return "", err
 	}
 
 	var accessToken tokenResponse
 	err = json.Unmarshal(body, &accessToken)
 	if err != nil {
 		log.Println(fmt.Errorf("Error unmarshalling body %s", err.Error()))
-		c.JSON(500, err)
-		return
+		return "", err
 	}
 
-	c.JSON(200, accessToken)
+	return accessToken.AccessToken, nil
 }
